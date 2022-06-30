776dc64a900e7f3214964c7ceb6dec3eee2b7fcd
stefanom13 <stefanomancuso13@gmail.com>
2022-06-30 14:20:44 +0200

    creazione classi

--- Program.cs
+++ Program.cs
@@ -19,3 +19,6 @@
 //Per i libri si ha in aggiunta il numero di pagine, mentre per i dvd la durata.
 //L’utente deve poter eseguire delle ricerche per codice o per titolo e, eventualmente, effettuare dei prestiti registrando il periodo (Dal/Al) del prestito e il documento.
 //Deve essere possibile effettuare la ricerca dei prestiti dato nome e cognome di un utente.
+
+using csharp_biblioteca;
+
